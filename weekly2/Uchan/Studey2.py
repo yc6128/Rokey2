186fce88813dd2e3cd9bb7126df36f3c4e38b5b5
yc6128 <126305815+yc6128@users.noreply.github.com>
2024-10-02 08:35:17 +0900

    Add files via upload

---
+++ weekly2/Uchan/Studey2.py
@@ -0,0 +1,40 @@
+
+#2
+s=3456
+s_len=len(s)
+if s_len == 4 and s_len == 6:
+    if type(s) == int:
+        print(True)
+    else:
+else:
+    print(False)
+    
+
+
+
+
+
+
+#3
+"""
+Num1=input("첫번째 숫자")
+Num2=input("두번쨰 숫자")
+
+def calculation(a,b):
+    if a%2==0:
+        result1=0
+        if
+        
+    else:
+   """     
+#4   
+n=int(input("가로길이"))
+m=int(input("세로길이"))
+i=0
+while i <= m:
+    print(("*")*n)
+    i+=1
+    
+        
+    
+    
