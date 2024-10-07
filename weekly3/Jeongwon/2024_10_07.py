177e1eea1fd5d5706aa51949a17192d6240f77fc
jeongwon-Yang <ghtjd3795@naver.com>
2024-10-07 19:07:17 +0900

    Update 2024_10_07.py

--- weekly3/Jeongwon/2024_10_07.py
+++ weekly3/Jeongwon/2024_10_07.py
@@ -21,11 +21,24 @@ def solution(s):
 print(solution("(()("))
 
 #problem 2
+'''
 def solution(brown, yellow):
     extent = brown + yellow
     for i in range((brown//2)-1, 0, -1):
         if extent % i ==0:
             return [i, extent//i]
+
+
+print(solution(10,2))
+'''
+
+def solution(brown, yellow):
+    extent = brown + yellow
+    for i in range(1, int(extent**0.5)+1):
+        if extent % i ==0:
+            j = extent // i
+            if (j - 2) * (i - 2) == yellow:
+                return [j, i]
 
 
 print(solution(10,2))
